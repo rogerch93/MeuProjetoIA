921973178dc7477d3c64ab9996c8ed4d390c3426
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-02-08 09:36:34 -0300

    Ajuste - Testes de autenticação

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using MeuProjetoIA.Features.HelloWorld;
 using MeuProjetoIA.Data;
 using MeuProjetoIA.Features.Mensagens;
 using MeuProjetoIA.Services;
 using MeuProjetoIA.Middleware;
+using MeuProjetoIA.Auth;
+using MeuProjetoIA.Features.Auth;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +22,26 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API IA Aprendizado", Version = "v1" });
 });
+
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+//{
+ //   var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>()
+ //                   ?? throw new InvalidOperationException("JwtSettings não está configurado corretamente.");
+ //   options.TokenValidationParameters = new TokenValidationParameters
+ //   {
+ //       ValidateIssuer = true,
+ //       ValidateAudience = true,
+ //       ValidateLifetime = true,
+ //       ValidateIssuerSigningKey = true,
+ //       ValidIssuer = jwtSettings.Issuer,
+ //       ValidAudience = jwtSettings.Audience,
+ //       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecuretKey))
+//};
+//});
+
+builder.Services.AddAuthorization();  // Adiciona serviços de autorização (mesmo que não tenhamos políticas específicas ainda)
 
 // Adicionar SQLite + EF Core
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -45,8 +71,7 @@ app.MapGet("/debug-env", () => new
 
 // Carregar endpoints de features 
 app.MapHelloWorldEndpoints();  
-
 app.MapMensagensEndpoints();
-
+app.MapAuthEndpoints();
 
 app.Run();
